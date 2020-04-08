9909ca1dbf187c9262768f0130d951c78940d632
sid221 <sudhanshu221b@gmail.com>
2020-04-08 10:23:43 +0530

    Added reusable search bar

--- src/components/pages/animeLibrary.js
+++ src/components/pages/animeLibrary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import SearchBar from "../assets/reusable/searchBar";
 
 // import getWindowSize from "../../assets/getWindowSize";
 import { theme } from "../../theme/dark";
@@ -8,31 +9,6 @@ const StyledAnimeLibrary = styled.div`
   display: flex;
   flex-direction: column;
   color: ${theme.darkColor1};
-  .search-container {
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    input {
-      width: 80%;
-      background-color: #aaaaaa;
-      height: 2.3rem;
-      font-size: 14px;
-      border: none;
-    }
-    button {
-      height: 2.5rem;
-      width: 3rem;
-      font-size: 14px;
-      border: none;
-      background: ${theme.darkBg3};
-      img {
-        z-index: 10;
-        height: 1.7rem;
-        width: 1.7rem;
-        filter: brightness(200%) invert(1);
-      }
-    }
-  }
 `;
 
 const AnimeLibrary = () => {
@@ -50,16 +26,11 @@ const AnimeLibrary = () => {
   return (
     <StyledAnimeLibrary>
       <div>Anime Library Page</div>
-      <div className="search-container">
-        <input
-          onChange={handleChange}
-          value={searchTerm}
-          placeholder="Enter the Anime you want to search"
-        />
-        <button onClick={handleSearch} className="search-button">
-          <img src="/static/images/search.svg" alt=">" />
-        </button>
-      </div>
+      <SearchBar
+        handleSearch={handleSearch}
+        handleChange={handleChange}
+        placeholder="Enter the Anime you want to search"
+      />
       <div className="search-keyword-desc">{`You have searched for anime "${searchTerm}"`}</div>
     </StyledAnimeLibrary>
   );
