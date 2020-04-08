705616e293fd993cf78640435498572e04a35579
sid221 <sudhanshu221b@gmail.com>
2020-04-08 10:22:18 +0530

    Added search bar reusable component

---
+++ src/components/assets/reusable/searchBar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+
+import { theme } from "../../../theme/dark";
+
+const StyledSearchBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-bottom: 0.7rem;
+  input {
+    width: 100%;
+    margin: 0 1.2rem 0 1rem;
+    height: 2.3rem;
+    position: relative;
+    padding-left: 0.5rem;
+    background-color: #aaaaaa;
+    font-size: 18px;
+    color: #0f0f0f;
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    position: absolute;
+    height: 2.5rem;
+    width: 3rem;
+    right: 1.2rem;
+    border: none;
+    text-align: center;
+    font-size: 14px;
+    background: ${theme.darkBg3};
+    &:focus {
+      outline: none;
+    }
+    img {
+      height: 1.7rem;
+      width: 1.7rem;
+      filter: brightness(200%) invert(1);
+    }
+  }
+`;
+
+const SearchBar = ({ handleChange, placeholder, handleSearch, defaultVal }) => {
+  return (
+    <StyledSearchBar>
+      <input
+        onChange={handleChange}
+        placeholder={placeholder}
+        defaultValue={localStorage.getItem(defaultVal)}
+      />
+      <button onClick={handleSearch} className="search-button">
+        <img src="/static/images/search.svg" alt=">" />
+      </button>
+    </StyledSearchBar>
+  );
+};
+
+export default SearchBar;
