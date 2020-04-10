f00328216bb0c93d70898eec3b0a2c4dd8d5b42c
sid221 <sudhanshu221b@gmail.com>
2020-04-11 04:07:47 +0530

    Added new shadow color

--- src/theme/dark.js
+++ src/theme/dark.js
@@ -6,6 +6,10 @@ const darkTheme = {
     darkBg1: "#101010",
     darkBg2: "#222222",
     darkBg3: "#2f2f2f",
+
+    // box shadow
+    darkSdw1: "#96ccc8c7",
+    
     // active Button
     darkBtn: "#2f2f2f",
     darkActiveBtn: "#080f36",
