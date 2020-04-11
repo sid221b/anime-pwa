46ebcf0b705f8e4898f2c3d31a54b63ff0f29340
sid221 <sudhanshu221b@gmail.com>
2020-04-11 16:08:26 +0530

    Add basic about us desc

--- src/components/pages/home/aboutUs.js
+++ src/components/pages/home/aboutUs.js
@@ -1,11 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 import HomeLayout from "../../../layout/home-layout/homeLayout";
+import { theme } from "../../../theme/dark";
+
+const StyledAboutUs = styled.div`
+    padding: 1.2rem;
+    font-size: 22px;
+    font-style: italic;
+    strong {
+      color: ${theme.darkColor3};
+      margin-top: 2rem;
+      display: block;
+    }
+`;
 
 const AboutUS = () => {
   return (
     <HomeLayout>
-      <div>Home About Page</div>
+      <StyledAboutUs>
+        There is nothing to about us just regular anime fans and anime
+        enthusiast. 
+
+        <strong>"MAYBE LATER WE WILL HAVE SOMETHING TO SAY ABOUT US"</strong>
+      </StyledAboutUs>
     </HomeLayout>
   );
 };
