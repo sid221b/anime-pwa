f8e3d948a5c14823474d26f4a75bac9b79a34375
sid221 <sudhanshu221b@gmail.com>
2020-04-17 19:47:55 +0530

    Added React google analytics dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect } from "react-router-dom";
+import ReactGA from "react-ga";
 
 import Layout from "./layout/layout";
 import DefaultRoute from "./routes/routes";
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize("UA-163886148-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Layout>
       {window.location.pathname === "/" && <Redirect to="/home" />}
